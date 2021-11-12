dbdab83e4540339df8a7a3cbc79fdc717a395f3d
Arturo Filastò <arturo@filasto.net>
2021-11-12 13:41:56 +0100

    Split out config vars

---
+++ android64.mk
@@ -0,0 +1,17 @@
+NDK_TOOLCHAIN_VERSION := clang
+HOST := aarch64-linux-android
+ALTHOST := $(HOST)
+GREP_CHECK := aarch64
+NDK_ABI := arm64
+NDK_BIT := 64
+NDK_PLATFORM_LEVEL := 21
+NDK_PROCESSOR := x86_64
+NDK_TOOLCHAIN := $(HOST)-$(NDK_TOOLCHAIN_VERSION)
+
+NDK_SYSROOT := $(ANDROID_NDK_HOME)/platforms/android-$(NDK_PLATFORM_LEVEL)/arch-$(NDK_ABI)
+NDK_UNAME := $(shell uname -s | tr '[A-Z]' '[a-z]')
+NDK_TOOLCHAIN_BASE := $(ANDROID_NDK_HOME)/toolchains/llvm/prebuilt/$(NDK_UNAME)-$(NDK_PROCESSOR)
+
+PATH := $(ANDROID_NDK_HOME)/toolchains/llvm/prebuilt/darwin-x86_64/bin:$(PATH)
+
+export CC := $(NDK_TOOLCHAIN_BASE)/bin/$(HOST)$(NDK_PLATFORM_LEVEL)-clang
