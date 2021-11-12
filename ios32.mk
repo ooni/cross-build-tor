43c7fdafd12d4181d7d4ab2a52eb85d172597d88
Arturo Filastò <arturo@filasto.net>
2021-11-12 15:36:12 +0100

    Add ios32 bit

---
+++ ios32.mk
@@ -0,0 +1,19 @@
+PLATFORM := iphoneos
+
+NDK_TOOLCHAIN_VERSION := clang
+
+ARCHITECTURE := armv7s
+HOST := arm-apple-darwin
+ALTHOST := arm-apple-darwin
+ISYSROOT := -isysroot $(shell xcrun -sdk $(PLATFORM) --show-sdk-path)
+MINVERSION_FLAG := -miphoneos-version-min=9.0
+
+OPENSSL_TARGET := ios64-cross
+
+export CC := $(shell xcrun -find -sdk $(PLATFORM) cc)
+export CXX := $(shell xcrun -find -sdk $(PLATFORM) g++)
+
+export CPPFLAGS := -arch $(ARCHITECTURE) $(ISYSROOT) $(CPPFLAGS)
+export CFLAGS := -O2 -arch $(ARCHITECTURE) $(MINVERSION_FLAG) $(CFLAGS)
+export CXXFLAGS := -O2 -arch $(ARCHITECTURE) $(MINVERSION_FLAG) $(CXXFLAGS)
+export LDFLAGS := -arch $(ARCHITECTURE) $(MINVERSION_FLAG) $(ISYSROOT) $(LDFLAGS)
